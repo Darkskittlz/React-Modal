bcd7bc125bcc9cf9f84046406a77bfa96f03c1b7
Jeremy <66503942+Darkskittlz@users.noreply.github.com>
2022-06-14 15:23:33 -0700

    Delete particlejs-react directory

--- particlejs-react/src/ParticleBackground.js
+++
@@ -1,8 +0,0 @@
-import Particles from "react-particles-js"
-import ParticleConfig from "./config/particle-config"
-
-export default function ParticleBackground() {
-    return (
-        <Particles params={ParticleConfig}></Particles>
-    )
-}
