018f5ab08cf8e96866b85f67f054a3feae563a12
Jeremy <66503942+Darkskittlz@users.noreply.github.com>
2022-06-14 15:23:24 -0700

    Delete React-Anastasia directory

--- React-Anastasia/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Navbar } from './Navbar/Navbar';
-export { default as Products } from './Products/Products';
-export { default as Cart } from './Cart/Cart';
-export { default as Updates } from './Updates/Updates';
-export { default as Gallery } from './Home/Home';
-export { default as Bookings } from './Bookings/Bookings';
-export { default as Prints } from './Prints/Prints';
